70689f5f8d8bb61d451affbfe0d7cd5d39986d5f
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2019-02-27 12:58:30 +0530

    alert color

--- step1.sh
+++ step1.sh
@@ -3,6 +3,9 @@ sudo sh ./scripts/setup-sudo.sh
 ls /etc/systemd/system/xema-manager.service
 if [ "$?" -eq "0" ]
 then
-  echo "Xema Node already installed."
+  red=`tput setaf 1`
+  green=`tput setaf 2`
+  reset=`tput sgr0`
+  echo "${green}Xema Node already installed.${reset}"
   echo "Continue to run step2.sh to reinstall ..."
 fi
