32448fde1bb639d752024bd207c6a3967b4da195
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2019-02-27 12:53:35 +0530

    reinstall alert

--- step1.sh
+++ step1.sh
@@ -1,1 +1,8 @@
 sudo sh ./scripts/setup-sudo.sh
+
+ls /etc/systemd/system/xema-manager.service
+if [ "$?" -eq "0" ]
+then
+  echo "Xema Node already installed."
+  echo "Continue to run step2.sh to reinstall ..."
+fi
