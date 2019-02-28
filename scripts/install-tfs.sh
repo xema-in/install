40583de82c368f1c73838bb9966d5a2d8393bab3
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2019-03-01 03:01:27 +0530

    bug fixes

--- scripts/install-tfs.sh
+++ scripts/install-tfs.sh
@@ -16,12 +16,7 @@ sudo -u xema -- sh -c '
 
 cd /home/xema
 
-mkdir azagent
-cd azagent
-curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.147.1/vsts-agent-linux-x64-2.147.1.tar.gz
-tar -zxvf vstsagent.tar.gz
-
-./config.sh --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname Xema --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga --runasservice
+mkdir azagent;cd azagent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.147.1/vsts-agent-linux-x64-2.147.1.tar.gz;tar -zxvf vstsagent.tar.gz; if [ -x "$(command -v systemctl)" ]; then ./config.sh --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga --runasservice; else ./config.sh --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga; ./run.sh; fi
 
 '
 
