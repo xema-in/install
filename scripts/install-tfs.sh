6426a3f2acbd8ab79f0e159db069541d406ca0b5
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2019-03-01 02:28:23 +0530

    tfs installation tweaks

--- scripts/install-tfs.sh
+++ scripts/install-tfs.sh
@@ -1,3 +1,9 @@
+id -u xema
+if [ "$?" -ne "0" ]
+then
+  sudo useradd xema -m
+fi
+
 
 which curl
 if [ "$?" -ne "0" ]
@@ -16,7 +22,8 @@ curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.14
 tar -zxvf vstsagent.tar.gz
 
 ./config.sh --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname Xema --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga --runasservice
-sudo ./svc.sh install xema
-sudo ./svc.sh start
 
 '
+
+sudo /home/xema/azagent/svc.sh install xema
+sudo /home/xema/azagent/svc.sh start
--- scripts/install-user.sh
+++
@@ -1,5 +0,0 @@
-id -u xema
-if [ "$?" -ne "0" ]
-then
-  useradd xema -m
-fi
