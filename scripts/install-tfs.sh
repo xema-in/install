0ae08b450fd971cfda6a204532d44345cd2bfdb2
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2019-03-01 12:11:01 +0530

    tfs agent installation tweaks

--- scripts/install-tfs.sh
+++ scripts/install-tfs.sh
@@ -1,7 +1,7 @@
 id -u xema
 if [ "$?" -ne "0" ]
 then
-  sudo useradd xema -m
+  sudo useradd -m -s /bin/bash xema
 fi
 
 
@@ -16,9 +16,11 @@ sudo -u xema -- sh -c '
 
 cd /home/xema
 
-mkdir azagent;cd azagent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.147.1/vsts-agent-linux-x64-2.147.1.tar.gz;tar -zxvf vstsagent.tar.gz; if [ -x "$(command -v systemctl)" ]; then ./config.sh --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga --runasservice; else ./config.sh --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga; ./run.sh; fi
+mkdir azagent;cd azagent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.147.1/vsts-agent-linux-x64-2.147.1.tar.gz;tar -zxvf vstsagent.tar.gz; if [ -x "$(command -v systemctl)" ]; then ./config.sh --unattended --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga --runasservice; else ./config.sh --unattended --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga; ./run.sh; fi
 
 '
 
-sudo /home/xema/azagent/svc.sh install xema
-sudo /home/xema/azagent/svc.sh start
+cd /home/xema/azagent
+
+sudo ./svc.sh install xema
+sudo ./svc.sh start
