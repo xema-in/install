4d1654acc0ca472e09dcd710aeac969613ad9d32
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2019-03-02 20:55:49 +0530

    final changes

--- scripts/install-tfs.sh
+++ scripts/install-tfs.sh
@@ -12,11 +12,41 @@ then
   sudo apt install -y curl
 fi
 
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+echo "${red}**********${reset}"
+echo "${red}Company Name and key are case sensitive.${reset}"
+echo "${red}Server Name should contains letters and numbers only. Space or any other special character not allowed.${reset}"
+echo "${red}Do not repeat server names.${reset}"
+echo "${red}**********${reset}"
+
+IFS="
+"
+echo "Enter your company name:"
+read company
+
+echo "Enter server name:"
+read server
+
+echo "Enter your key (PAT):"
+read pat
+
 sudo -u xema -- sh -c '
 
 cd /home/xema
 
-mkdir azagent;cd azagent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.147.1/vsts-agent-linux-x64-2.147.1.tar.gz;tar -zxvf vstsagent.tar.gz; if [ -x "$(command -v systemctl)" ]; then ./config.sh --unattended --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga --runasservice; else ./config.sh --unattended --deploymentgroup --deploymentgroupname "1100 Servers" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token fdt5a6p2k7hhrlex2lhuqx4yju6gxqzzwflmatsnh5wgn4kzd3ga; ./run.sh; fi
+mkdir azagent
+cd azagent
+wget -c https://vstsagentpackage.azureedge.net/agent/2.147.1/vsts-agent-linux-x64-2.147.1.tar.gz
+tar -zxvf vsts-agent-linux-x64-2.147.1.tar.gz
+if [ -x "$(command -v systemctl)" ]
+then 
+  ./config.sh --runasservice --unattended --acceptteeeula --deploymentgroup --deploymentgroupname "'$company'" --agent "'$server'" --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token '$pat'
+else 
+  ./config.sh                --unattended --acceptteeeula --deploymentgroup --deploymentgroupname "'$company'" --agent "'$server'" --url https://dev.azure.com/xema-in/ --work _work --projectname 'Xema' --auth PAT --token '$pat'
+  ./run.sh
+fi
 
 '
 
